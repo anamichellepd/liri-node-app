133a4a7ac6c95dedf5d47e59edee819b09e021c8
Ana Pineda <anamichellepd@gmail.com>
2020-03-02 21:57:39 -0800

    added the movie part. Still pending to create all defaults because I can't figure it out yet

--- liri.js
+++ liri.js
@@ -44,19 +44,19 @@ if (process.argv[2] === "concert-this") {
 } else if (process.argv[2] === "spotify-this-song") {
   var spotify = new Spotify(keys.spotify);
   var songName = [];
+
   for (var i = 3; i < process.argv.length; i++) {
     songName.push(process.argv[i]);
   }
   var songString = songName.join("+");
+
   var songNormal = songName.join(" ");
 
-  spotify.search({ type: "track", query: songString, limit: 5 }, function(
-    err,
-    data
-  ) {
+  spotify.search({ type: "track", query: songString }, function(err, data) {
     if (err) {
       return console.log("Error occurred: " + err);
     }
+
     console.log("Artist name: " + data.tracks.items[0].album.artists[0].name);
     console.log("Song name: " + songNormal);
     console.log(
@@ -64,10 +64,24 @@ if (process.argv[2] === "concert-this") {
     );
     console.log("The name of the album is: " + data.tracks.items[0].album.name);
   });
-  // } else if (process.argv[2] === "movie-this") {
-  //   var movieName = [];
-  //   for (var i = 3; i < process.argv.length; ++i) {
-  //     movieName.push(process.argv[i]);
-  //     var movieString = movieName.join("+");
-  //   }
+} else if (process.argv[2] === "movie-this") {
+  var movieName = [];
+  for (var i = 3; i < process.argv.length; ++i) {
+    movieName.push(process.argv[i]);
+    var movieString = movieName.join("+");
+    var queryURL =
+      "http://www.omdbapi.com/?t=" +
+      movieString +
+      "&y=&plot=short&apikey=trilogy";
+  }
+  axios.get(queryURL).then(function(response) {
+    console.log("Title: " + response.data.Title);
+    console.log("Release year: " + response.data.Year);
+    console.log("IMDb rating: " + response.data.imdbRating);
+    console.log("Rotten Tomatoes rating: " + response.data.Ratings[1].Value);
+    console.log("Country where movie was produced: " + response.data.Country);
+    console.log("Language of the movie: " + response.data.Language);
+    console.log("Movie plot: " + response.data.Plot);
+    console.log("Actors: " + response.data.Actors);
+  });
 }
